6b68e12e9592ee868d6c8c97a84ac4a4358de9c3
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2021-11-30 23:39:11 +0530

    Update repo.py

--- kannax/plugins/kawaii/repo.py
+++ kannax/plugins/kawaii/repo.py
@@ -1,35 +1,10 @@
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# Editado por fnixdev
+# repo
 
-from kannax import Message, kannax
+from kannax import Config, Message, kannax
 
 
-@kannax.on_cmd(
-    "repo",
-    about={
-        "header": "link e detalhes do repositório",
-    },
-)
+@kannax.on_cmd("repo", about={"header": "get repo link and details"})
 async def see_repo(message: Message):
-    bot = await kannax.bot.get_me()
-    x = await kannax.get_inline_bot_results(bot.username, "repo")
-    await kannax.send_inline_bot_result(
-        chat_id=message.chat.id, query_id=x.query_id, result_id=x.results[0].id
-    )
-    await message.delete()
-
-
-@kannax.on_cmd(
-    "string",
-    about={
-        "header": "link e detalhes do repositório",
-    },
-)
-async def see_session(message: Message):
-    bot = await kannax.bot.get_me()
-    x = await kannax.get_inline_bot_results(bot.username, "session")
-    await kannax.send_inline_bot_result(
-        chat_id=message.chat.id, query_id=x.query_id, result_id=x.results[0].id
-    )
-    await message.delete()
+    """see repo"""
+    output = f"• **repo** : [my repo]({Config.UPSTREAM_REPO})"
+    await message.edit(output)
