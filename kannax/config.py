60e06a235d1653f4a50500ecdcf7cea4ef167b9a
thegreatfoxxgoddess <thegreatfoxxgoddess@yandex.com>
2021-12-22 10:37:31 +0000

    auto: code check

--- kannax/config.py
+++ kannax/config.py
@@ -98,7 +98,7 @@ class Config:
     BOT_MEDIA = os.environ.get("BOT_MEDIA")
     SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
     SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
-    SPOTIFY_MODE = False 
+    SPOTIFY_MODE = False
     SPOTIFY_USERNAME = os.environ.get("SPOTIFY_USERNAME")
     IMGFLIP_ID = os.environ.get("IMGFLIP_ID")
     IMGFLIP_PASS = os.environ.get("IMGFLIP_PASS")
